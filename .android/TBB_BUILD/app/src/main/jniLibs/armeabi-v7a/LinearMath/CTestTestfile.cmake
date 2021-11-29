2796deb974b537b130a4c5ed102b86aeb70c0db6
Ignat <63556689+Tam1SH@users.noreply.github.com>
2021-11-30 02:25:49 +0300

    Delete .android directory

--- .android/TBB_BUILD/app/src/main/jniLibs/armeabi-v7a/LinearMath/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ignat/AndroidStudioProjects/TBB_BUILD/app/src/main/cpp/bullet3-3.17/src/LinearMath
-# Build directory: C:/Users/ignat/AndroidStudioProjects/TBB_BUILD/app/.cxx/cmake/debug/armeabi-v7a/bullet3-3.17/src/LinearMath
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- .android/TBB_BUILD/app/src/main/jniLibs/x86_64/BulletCollision/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ignat/AndroidStudioProjects/TBB_BUILD/app/src/main/cpp/bullet3-3.17/src/BulletCollision
-# Build directory: C:/Users/ignat/AndroidStudioProjects/TBB_BUILD/app/.cxx/cmake/debug/x86_64/bullet3-3.17/src/BulletCollision
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
